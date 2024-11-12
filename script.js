43952fff17328afac834858f4b2a930480a806c8
perniep <pernillap94@hotmail.com>
2024-11-12 16:03:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+// let text = "";
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2) {
+//     if (i >= 5) text += "Q";
+//     else {
+//       text += "O";
+//     }
+//   } else {
+//     text += "X";
+//   }
+// }
+// console.log(text);
+
+
+
+//console.log(citynames[0][0])
+// Assignment 2 c
+// let citynames = ["kiruna", "uppsala", "malmo", "gothenburg", "stockholm"];
+
+
+// for (let i = 0; i < citynames.length; i += 1) {
+    
+//         for (let i2 = 0; i2 < citynames[i].length; i2 += 1) 
+//             console.log(citynames[i][i2])
+     
+    
+           
+      
+// }
+
+
+for (let index = 0; index < 5; index++) {
+console.log(index*3)
+    
+}
